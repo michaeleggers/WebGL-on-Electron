e5955eb5eb283e9ce1340dfc92de0ac514daa37e
Michael Eggers <mseggers@protonmail.ch>
2022-09-25 12:56:10 +0200

    try to make open gl canvas same size as window

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function main() {
     const canvas = document.getElementById("glCanvas");
+    
+    // Resize to current window width/height
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
     // Initialize the GL context
     const gl = canvas.getContext("webgl");
-  
     // Only continue if WebGL is available and working
     if (gl === null) {
       alert("Unable to initialize WebGL. Your browser or machine may not support it.");
@@ -12,8 +16,15 @@ function main() {
     let red = 0.0
     let addRed = 1
     function drawFrame(nowTime) {
+
+        window.onresize = (evnt) => {
+            canvas.width = window.innerWidth
+            canvas.height = window.innerHeight
+        }
+
         // Set clear color to black, fully opaque
         gl.clearColor(red, 0.9, 0.0, 1.0);
+        // gl.viewport(0, 0, 10, 10)
         if (red > 1.0) {
             addRed = -1
         }
